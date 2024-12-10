5daa2a694ffeef4bef7a3c63c336996ac913b062
curso00 <curso00@nomail.com>
2024-12-10 11:11:07 +0100

    Terminadas las operaciones de delete, post y put

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,6 +3,7 @@ $(function() {
     $("#listar").on("click", function() {
         $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes", function(data) {
             $("#resListar").text("Ok");
+            $("#resListar").attr("data-midato", data);
             console.log(data);
         })
         
@@ -13,5 +14,59 @@ $(function() {
             console.log(data);
         })
         
-    })
+    });
+
+
+    $('#crear').on('click',function() {
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes",
+            method: "POST",
+            "data": JSON.stringify({
+                id: 0,
+                nombre: "Juan",
+                apellido: "Otro"
+            }),
+            success: function(data) {
+                $("#resCrear").text("Ok"); 
+                console.log(data);
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    });
+
+
+    $('#actualizar').on('click',function() {
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/2",
+            method: "PUT",
+            "data": JSON.stringify({
+                id: 1,
+                nombre: "Juan",
+                apellido: "Otro"
+            }),
+            success: function(data) {
+                $("#resActualizar").text("Ok"); 
+                console.log(data);
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    });
+
+    $('#borrar').on('click',function() {
+        $.ajax({
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/1",
+            method: "DELETE",
+            success: function(data) {
+                $("#resBorrar").text("Ok"); 
+                console.log(data);
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        });
+    });
 });
